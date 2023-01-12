dbedd2cfec72f19196ee25728a2ccfc4338d7f58
Jurre Groenendijk <j.j.k.groenendijk@student.tudelft.nl>
2023-01-12 23:00:05 +0100

    Implement plugin

--- src/main/java/com/handegg/HandeggDiscordWebhookConfig.java
+++ src/main/java/com/handegg/HandeggDiscordWebhookConfig.java
@@ -3,17 +3,68 @@ package com.handegg;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("handeggdiscordwebhook")
 public interface HandeggDiscordWebhookConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+
+	@ConfigSection(
+		name = "Webhook Settings",
+		description = "The config for webhook content notifications",
+		position = 0,
+		closedByDefault = true
 	)
-	default String greeting()
-	{
-		return "Hello";
+	String webhookConfig = "webhookConfig";
+
+	@ConfigItem(
+			keyName = "webhook",
+			name = "Webhook URL(s)",
+			description = "The Discord Webhook URL(s) to send messages to, separated by a newline.",
+			section = webhookConfig,
+			position = 0
+	)
+	String webhook();
+
+
+	@ConfigSection(
+			name = "Message Settings",
+			description = "The config for when and how to send messages.",
+			position = 1,
+			closedByDefault = false
+	)
+	String messageConfig = "messageConfig";
+
+	@ConfigItem(
+			keyName = "includeScreenshot",
+			name = "Include Screenshot",
+			description = "Add a screenshot to the message.",
+			section = messageConfig,
+			position = 0
+	)
+	default boolean includeScreenshot() {
+		return true;
 	}
+	@ConfigItem(
+			keyName = "uploadOnReceive",
+			name = "Send Message When Handegged",
+			description = "Send a message when you get handegged by another player.",
+			section = messageConfig,
+			position = 1
+	)
+	default boolean uploadOnReceive() {
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "uploadOnThrow",
+			name = "Send Message When You Handegg",
+			description = "Send a message when you handegg another player.",
+			section = messageConfig,
+			position = 2
+	)
+	default boolean uploadOnThrow() {
+		return false;
+	}
+
 }
--- src/main/java/com/handegg/HandeggDiscordWebhookPlugin.java
+++ src/main/java/com/handegg/HandeggDiscordWebhookPlugin.java
@@ -1,16 +1,31 @@
 package com.handegg;
 
+import com.google.common.base.Strings;
 import com.google.inject.Provides;
+
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
+import net.runelite.api.events.AnimationChanged;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.DrawManager;
+import okhttp3.*;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static net.runelite.http.api.RuneLiteAPI.GSON;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,36 +33,202 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class HandeggDiscordWebhookPlugin extends Plugin
 {
+	public static int HANDEGG_THROWN_ANIMATIONID = 7995;
+	public static int HANDEGG_RECEIVED_ANIMATIONID = 782;
+  public static HandeggState state = HandeggState.IDLE;
+
+  private static int currentGameTick = 0;
+  private static int lastStateChanged;
+	private static String lastThrower = "";
+	private static String lastReciever = "";
+	private static String currentPlayer;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private HandeggDiscordWebhookConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Handegg Discord Webhook started!");
-	}
+	@Inject
+	private DrawManager drawManager;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Handegg Discord Webhook stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Handegg Discord Webhook says " + config.greeting(), null);
-		}
-	}
+	@Inject
+	private OkHttpClient okHttpClient;
 
 	@Provides
 	HandeggDiscordWebhookConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(HandeggDiscordWebhookConfig.class);
 	}
+
+	private void updateState(HandeggState newState){
+		state = newState;
+		lastStateChanged = currentGameTick;
+	}
+
+	@Subscribe
+	public void onAnimationChanged(AnimationChanged animationChanged)
+	{
+		if(currentPlayer == null) {
+			Player currentPlayerFromClient = client.getLocalPlayer();
+			if (currentPlayerFromClient == null) return;
+			String currentPlayerName = currentPlayerFromClient.getName();
+			if (currentPlayerName == null) return;
+			currentPlayer = currentPlayerName;
+		}
+
+		String actorName = animationChanged.getActor().getName();
+
+		int changedAnimationID = animationChanged.getActor().getAnimation();
+
+		if(changedAnimationID == HANDEGG_THROWN_ANIMATIONID){
+
+			// If I'm the one who threw the handegg
+			if(currentPlayer.equals(actorName)){
+				updateState(HandeggState.THROWN);
+			} else {
+				lastThrower = actorName;
+			}
+
+		} else if(changedAnimationID == HANDEGG_RECEIVED_ANIMATIONID){
+
+			// If I'm the one who just got handegged
+			if(currentPlayer.equals(actorName)){
+				updateState(HandeggState.RECEIVED);
+			} else {
+				lastReciever = actorName;
+			}
+
+		}
+	}
+
+	// Using gametick as a mini internal clock
+	@Subscribe
+	public void onGameTick(GameTick event)
+	{
+		currentGameTick++;
+
+		// If 3 game ticks have passed since the last state change
+		if(state != HandeggState.IDLE && lastStateChanged + 3 < currentGameTick){
+			if(state == HandeggState.RECEIVED){
+
+				String message = currentPlayer +" just got handegged by "+lastThrower+"!";
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "You just got handegged by "+lastThrower+"!", null);
+				log.info(message);
+				if(config.uploadOnReceive()){
+					sendWebhook(message);
+				}
+			}
+			if(state == HandeggState.THROWN){
+				String message = currentPlayer +" just handegged "+lastReciever+"!";
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "You just handegged "+lastReciever+"!", null);
+				log.info(message);
+				if(config.uploadOnThrow()) {
+					sendWebhook(message);
+				}
+			}
+			updateState(HandeggState.IDLE);
+		}
+	}
+
+	private void sendWebhook(String message){
+		DiscordWebhookBody discordWebhookBody = new DiscordWebhookBody();
+		discordWebhookBody.setContent(message);
+		sendWebhook(discordWebhookBody, config.includeScreenshot());
+
+	}
+
+	private void sendWebhook(DiscordWebhookBody discordWebhookBody, boolean sendScreenshot)
+	{
+		String configUrl = config.webhook();
+		if (Strings.isNullOrEmpty(configUrl)) { return; }
+
+		List<String> webhookUrls = Arrays.asList(configUrl.split("\n"))
+				.stream()
+				.filter(u -> u.length() > 0)
+				.map(u -> u.trim())
+				.collect(Collectors.toList());
+
+		for (String webhookUrl : webhookUrls)
+		{
+			HttpUrl url = HttpUrl.parse(webhookUrl);
+			MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder()
+					.setType(MultipartBody.FORM)
+					.addFormDataPart("payload_json", GSON.toJson(discordWebhookBody));
+
+			if (sendScreenshot)
+			{
+				sendWebhookWithScreenshot(url, requestBodyBuilder);
+			}
+			else
+			{
+				buildRequestAndSend(url, requestBodyBuilder);
+			}
+		}
+	}
+
+	private void sendWebhookWithScreenshot(HttpUrl url, MultipartBody.Builder requestBodyBuilder)
+	{
+		drawManager.requestNextFrameListener(image ->
+		{
+			BufferedImage bufferedImage = (BufferedImage) image;
+			byte[] imageBytes;
+			try
+			{
+				imageBytes = convertImageToByteArray(bufferedImage);
+			}
+			catch (IOException e)
+			{
+				log.warn("Error converting image to byte array", e);
+				return;
+			}
+
+			requestBodyBuilder.addFormDataPart("file", "image.png",
+					RequestBody.create(MediaType.parse("image/png"), imageBytes));
+			buildRequestAndSend(url, requestBodyBuilder);
+		});
+	}
+
+	private void buildRequestAndSend(HttpUrl url, MultipartBody.Builder requestBodyBuilder)
+	{
+		RequestBody requestBody = requestBodyBuilder.build();
+		Request request = new Request.Builder()
+				.url(url)
+				.post(requestBody)
+				.build();
+		sendRequest(request);
+	}
+
+	private void sendRequest(Request request)
+	{
+		okHttpClient.newCall(request).enqueue(new Callback()
+		{
+			@Override
+			public void onFailure(Call call, IOException e)
+			{
+				log.debug("Error submitting webhook", e);
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException
+			{
+				response.close();
+			}
+		});
+	}
+
+	private static byte[] convertImageToByteArray(BufferedImage bufferedImage) throws IOException
+	{
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+		return byteArrayOutputStream.toByteArray();
+	}
+
+
+}
+
+enum HandeggState{
+	IDLE,
+	THROWN,
+	RECEIVED
 }
